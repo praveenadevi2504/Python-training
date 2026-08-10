8fbd518c1d32164dfc806a6d2452a3fd6bea9351
praveenadevi2504 <praveenadevi@gmail.com>
2026-08-10 11:00:04 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+print("==============================")
+print("      BASIC CALCULATOR")
+print("==============================")
+
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = int(input("Enter your choice: "))
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if choice == 1:
+    print("Addition =", a + b)
+
+elif choice == 2:
+    print("Subtraction =", a - b)
+
+elif choice == 3:
+    print("Multiplication =", a * b)
+
+elif choice == 4:
+    print("Division =", a / b)
+
+else:
+    print("Invalid choice")
